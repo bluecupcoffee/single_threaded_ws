b400507a5e7da23cc2c1a11164d0d4c055b64b1c
bluecupcoffee <coffeeincodeout@proton.me>
2024-07-17 20:06:54 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    process
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878");
+    if let Ok(tl) = listener {
+        println!("Listener succesfully created");
+        for stream in tl.incoming() {
+            let stream = stream.unwrap();
+
+            handle_connection(stream);
+        }
+    } else if let Err(e) = listener {
+        eprintln!("Error when attempting to bind to address: {:?}", e);
+        process::exit(1);
+    }
+
+    println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+}
